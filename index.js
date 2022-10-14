a90c17a9c24e609a960a322cc77f40f98534e965
Amereen Hanvel <amereenhanvel786@gmail.com>
2022-10-14 14:21:38 -0400

    Console.logged my data to see an array of objects in my dev tools console

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
 	fetch('http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline')
+  .then((response) => response.json())
+  .then((data) => {
+    console.log('Data', data);
+  })
+
 })
