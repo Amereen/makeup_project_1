b169e17588535a61383316e68efde73e9872b8be
Amereen Hanvel <amereenhanvel786@gmail.com>
2022-10-16 20:35:55 -0400

    made a liked btn create element

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ document.addEventListener('DOMContentLoaded', () => {
 function inputData(makeupInfo) {
   const divCard = document.createElement('div');
 	// const btn = document.createElement('button');
-	const liked = document.createElement('div');
+	// const liked = document.createElement('div');
 	const name = document.createElement('div');
 	const img = document.createElement('img');
 	const desc = document.createElement('div');
