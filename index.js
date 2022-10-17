9d8f03d9e15bd722462400291bd6cbe868a0ea6a
Amereen Hanvel <amereenhanvel786@gmail.com>
2022-10-16 20:37:13 -0400

    created my dom content loaded and a fetch request to my api

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
-document.addEventListener('DOMContentLoaded', () => {
-	fetch('http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline')
-  .then((response) => response.json())
-  .then((data) => {
-    console.log('Data', data);
-  })
-  data.forEach((obj) => {
-    inputData(obj);
-    console.log('category')
-    });
+// document.addEventListener('DOMContentLoaded', () => {
+// 	fetch('http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline')
+//   .then((response) => response.json())
+//   .then((data) => {
+//     console.log('Data', data);
+//   })
+//   data.forEach((obj) => {
+//     inputData(obj);
+//     console.log('category')
+//     });
     
-});
+// });
 
 function inputData(makeupInfo) {
   const divCard = document.createElement('div');
-	// const btn = document.createElement('button');
-	// const liked = document.createElement('div');
+	const btn = document.createElement('button');
+	const liked = document.createElement('div');
 	const name = document.createElement('div');
 	const img = document.createElement('img');
 	const desc = document.createElement('div');
 	const price = document.createElement('div');
 
-  // comment test
+
 
 }
